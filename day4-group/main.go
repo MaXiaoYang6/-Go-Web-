c79faf6abede1e9daf533dc01240f28bc1b255ea
mxy <xiaoyangma@mail.ustc.edu.cn>
2022-11-18 20:22:21 +0800

    完善了main函数

--- day4-group/main.go
+++ day4-group/main.go
@@ -1,7 +1,9 @@
 package main
 
-import "net/http"
-import "Gee/day4-group/gee"
+import (
+	"Gee/day4-group/gee"
+	"net/http"
+)
 
 func main() {
 	r := gee.New()
@@ -13,26 +15,24 @@ func main() {
 		v1.GET("/", func(c *gee.Context) {
 			c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
 		})
-
 		v1.GET("/hello", func(c *gee.Context) {
-			// expect /hello?name=geektutu
+			// expect /hello?name=mxy
 			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
 		})
 	}
 	v2 := r.Group("/v2")
 	{
 		v2.GET("/hello/:name", func(c *gee.Context) {
-			// expect /hello/geektutu
-			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+			// expect /hello/mxy
+			c.String(http.StatusOK, "hello %s, you are at %s\n", c.Param("name"), c.Path)
 		})
+
 		v2.POST("/login", func(c *gee.Context) {
 			c.JSON(http.StatusOK, gee.H{
 				"username": c.PostForm("username"),
 				"password": c.PostForm("password"),
 			})
 		})
-
 	}
-
 	r.Run(":9999")
 }
